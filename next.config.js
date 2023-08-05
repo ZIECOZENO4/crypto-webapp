fdec88d46f9d688e7b214cfec691b9521e82d509
ZIECOZENO4 <ziecozeno4@gmail.com>
2023-08-05 21:10:48 +0100

    TUUUU

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
+  output: 'export',
   env: {
     GOOGLE_CLIENT_ID: "1050309461907-h7cn8aovje8qi33tonhmv2o2d0r3i0dm.apps.googleusercontent.com",
     GOOGLE_CLIENT_SECRET: "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",
     GITHUB_ID: "b7ff10e7cb67e9cc24d8",
-   GITHUB_SECRET: "65004e503988e51e6c53be58eb43a734d6d1c373",
-   DISCORD_CLIENT_ID: "1135024897266028746",
-   DISCORD_CLIENT_SECRET: "C9kRY3Z8nUyREPIBpkUa-SgsUfmmgMA8"
-
+    GITHUB_SECRET: "65004e503988e51e6c53be58eb43a734d6d1c373",
+    DISCORD_CLIENT_ID: "1135024897266028746",
+    DISCORD_CLIENT_SECRET: "C9kRY3Z8nUyREPIBpkUa-SgsUfmmgMA8"
   },
-
   images: {
-    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com',  'cdn.discordapp.com'],
+    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com', 'cdn.discordapp.com'],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
