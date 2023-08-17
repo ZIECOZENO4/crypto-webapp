1c340efc2b09dba02d62bcd712a08a53385bbcfe
ZIECOZENO4 <ziecozeno4@gmail.com>
2023-08-17 14:39:59 +0100

    signout

--- app/sidebar/page.tsx
+++ app/sidebar/page.tsx
@@ -124,8 +124,8 @@ const profileVariants = {
   <Items icon={<FcSalesPerformance/>} name='Performance' />
 
       
-{/* <button onClick={() => signOut({ callbackUrl: '/signin' })}>Sign Out</button> */}
- 
+  <button onClick={() => signOut({ callbackUrl: '/signin' })}>Sign Out</button>
+
 
 </div>
           </div>
